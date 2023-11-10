f9f678e28cd09dc0a00baf8243776d061827345b
RayanEreteo <rayabf5@gmail.com>
2023-11-10 14:28:49 +0100

    ajout du CORS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const account_manager = require("./auth/account_manager")
 const body_parser = require("body-parser")
+const cors = require("cors")
 
 const app = express()
+app.use(cors())
 app.use(body_parser.json())
 
 
