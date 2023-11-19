5d32918f6ce8e48568cb1a48e6272a55f49941c4
RayanEreteo <rayabf5@gmail.com>
2023-11-19 11:24:17 +0100

    ajout du module "dotnev"

--- auth/account_manager.js
+++ auth/account_manager.js
@@ -92,7 +92,7 @@ async function login(req, res) {
     });
   }
 
-  const token = jwt.sign({email: email}, "test", {
+  const token = jwt.sign({email: email}, sk, {
     expiresIn: "2d"
   })
 
--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express")
 const body_parser = require("body-parser")
 const cors = require("cors")
+require('dotenv').config()
 
 const email_verifier = require("./auth/email_verifier")
 const account_manager = require("./auth/account_manager")
 const db_conn = require("./db_conn");
 const token_verify = require("./auth/token_verify")
 
-
 const app = express()
 app.use(cors())
 app.use(body_parser.json())
-
 
 // connexion a la base de donnée
 db_conn();
@@ -23,7 +22,6 @@ app.post("/login", account_manager.login)
 app.post("/tokenchecker", token_verify, (req, res) => {
     res.send("ressource autorisée, utilisateur : " + req.body.decoded.email)
 })
-
 
 app.get("/verify", email_verifier)
 
